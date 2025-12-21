003be914bccecf4d32d6aa0184e4bab41da1fe0d
foozld <tylerbuno@gmail.com>
2025-12-21 18:31:43 -0500

    linked list methods

--- src/datastructures/list/LinkedList.java
+++ src/datastructures/list/LinkedList.java
@@ -15,5 +15,85 @@ public class LinkedList<T> {
 	private Node<T> tail;
 	private int size;
 	
+	public LinkedList() {
+		head = null;
+		tail = null;
+		size = 0;
+	}
 	
+	public void addHead(T data) {
+		Node<T> newNode = new Node<>(data);
+		if (isEmpty()) {
+			head = newNode;
+			tail = newNode;
+			size++;
+		} else {
+			newNode.next = head;
+			head = newNode;
+		}
+		size++;
+	}
+	
+	public void addTail(T data) {
+		Node<T> newNode = new Node<>(data);
+		if (isEmpty()) {
+			head = newNode;
+			tail = newNode;
+			size++;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+		size++;
+	}
+	
+	public T removeHead() {
+		if (isEmpty()) {
+			return null;
+		}
+		
+		T value = head.data;
+		head = head.next;
+		size--;
+		
+		if (isEmpty()) {
+			tail = null;
+		}
+		return value;
+	}
+	
+	public T removeTail() {
+		T value = tail.data;
+				
+		return value;
+	}
+	
+	public boolean contains(T data) {
+		Node<T> curr = head;
+		
+		while (curr != null) {
+			if (curr.data.equals(data)) {
+				return true;
+			}
+			curr = curr.next;
+		}
+		return false;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public void printList() {
+		Node<T> curr = head;
+		while (curr != null) {
+			System.out.println(curr.data + "");
+			curr = curr.next;
+		}
+		System.out.println("null");
+	}
 }//list class
