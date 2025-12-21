61225daa676f408ff86d97dd08158e26bab6d28d
foozld <tylerbuno@gmail.com>
2025-12-21 17:55:35 -0500

    experimental commit, node class

--- src/datastructures/list/LinkedList.java
+++ src/datastructures/list/LinkedList.java
@@ -1,5 +1,19 @@
 package datastructures.list;
 
-public class LinkedList {
-
-}
+public class LinkedList<T> {
+	private static class Node<T> {
+		T data;
+		Node<T> next;
+		
+		Node (T data) {
+			this.data = null;
+			this.next = null;
+		}
+	}//node class
+	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size;
+	
+	
+}//list class
