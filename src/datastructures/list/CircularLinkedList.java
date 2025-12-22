6b47ec80ba614689f2b3af8c764979e82da574cb
foozld <tylerbuno@gmail.com>
2025-12-22 18:19:14 -0500

    initializing circular linked list class

--- src/datastructures/list/CircularLinkedList.java
+++ src/datastructures/list/CircularLinkedList.java
@@ -1,5 +1,23 @@
 package datastructures.list;
 
-public class CircularLinkedList {
-
-}
+public class CircularLinkedList<T> {
+	private static class Node<T> {
+		T data;
+		Node<T> next;
+		
+		Node(T data) {
+			this.data = data;
+			this.next = null;
+		}
+	}//node
+	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size;
+	
+	public CircularLinkedList() {
+		head = null;
+		tail = null;
+		size = 0;
+	}
+}//circular linked list
