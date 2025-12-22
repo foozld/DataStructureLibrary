dc48a951ed34f59ce2a90da52354d15e0aed3051
foozld <tylerbuno@gmail.com>
2025-12-22 17:06:21 -0500

    initializing doubly linked list

--- src/datastructures/list/DoublyLinkedList.java
+++ src/datastructures/list/DoublyLinkedList.java
@@ -1,5 +1,27 @@
 package datastructures.list;
 
-public class DoublyLinkedList {
-
-}
+public class DoublyLinkedList<T> {
+	private static class Node<T> {
+		T data;
+		Node<T> next;
+		Node<T> prev;
+		
+		Node(T data) {
+			this.data = data;
+			this.next = null;
+			this.prev = null;
+		}
+	}//node
+	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size;
+	
+	public DoublyLinkedList() {
+		head = null;
+		tail = null;
+		size = 0;
+	}
+	
+	
+}//doubly linked list class
