e5d69cf03e6ed1d1883072b12c8e556b74a28378
foozld <tylerbuno@gmail.com>
2025-12-22 18:15:01 -0500

    doubly linked list methods

--- src/datastructures/list/DoublyLinkedList.java
+++ src/datastructures/list/DoublyLinkedList.java
@@ -23,5 +23,83 @@ public class DoublyLinkedList<T> {
 		size = 0;
 	}
 	
+	public void addHead(T data) {
+		Node<T> newNode = new Node<T>(data);
+		if (isEmpty()) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			newNode.next = head;
+			head.prev = newNode;
+			head = newNode;
+		}
+		size++;
+	}
 	
+	public void addTail(T data) {
+		Node<T> newNode = new Node<T>(data);
+		if (isEmpty()) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			newNode.prev = tail;
+			tail.next = newNode;
+			tail = newNode;
+		}
+		size++;
+	}
+	
+	public T removeHead() {
+		if (isEmpty()) {
+			return null;
+		} 
+		T value = head.data;
+		
+		if (size == 1) {
+			head = null;
+			tail = null;
+		} else {
+			head = head.next;
+			head.prev = null;
+		}
+		
+		size--;
+		return value;
+	}
+	
+	public T removeTail() {
+		if (isEmpty()) {
+			return null;
+		}
+		T value = tail.data;
+		
+		if (size == 1) {
+			head = null;
+			tail = null;
+		} else {
+			tail = tail.prev;
+			tail.next = null;
+		}
+		size--;
+		return value;
+	}
+	
+	public boolean contains(T data) {
+		Node<T> curr = head;
+		
+		while (curr != null) {
+			if (curr.data.equals(data)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
 }//doubly linked list class
